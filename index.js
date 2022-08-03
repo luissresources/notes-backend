7b0942fb73ef726e78135b1df6c08d7567d83765
Luiss Resources <luissresources@gmail.com>
2022-08-03 12:38:18 -0500

    update post, put. Added errorHandle

--- index.js
+++ index.js
@@ -51,10 +51,16 @@ app.get('/api/notes',(request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -103,8 +109,8 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = request.params.id
+app.put('/api/notes/:id', (request, response, next) => {
+  /* const id = request.params.id
   const content = request.body.content
   const important = request.body.important
   Note.findOne({_id: id})
@@ -119,26 +125,61 @@ app.put('/api/notes/:id', (request, response) => {
           })
         })
         .catch(error => {
-          console.log('Error in update')
-          response.status(400).send({
-            message: 'Update error',
-            error
-          })
+          console.log({error})
+          // response.status(400).send({
+          //   message: 'Update error',
+          //   error
+          // })
+          next(error)
         })
-    })
+    }) */
 
+    const body = request.body
 
-  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
 
+    // new: true returns the updated object
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = req.params.id
-  Note.deleteOne({_id: id}).then(note => {
-    console.log({note})
-    res.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    // the result is the same whether it exists or not. could be changed
+    .then(result => {
+      console.log({result})
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log({error})
+      next(error)
     })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
